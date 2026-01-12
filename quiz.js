c7c9b72a6efd1682dfec6b52b33293c2974f4d23
syntax-elpadrino <eduardoeduuh@proton.me>
2026-01-12 22:00:23 +0300

    Implement a simple quiz app

---
+++ quiz.js
@@ -0,0 +1,22 @@
+function checkAnswer() {
+    const correctAnswer = "4";
+
+    const selected = document.querySelector('input[name="quiz"]:checked');
+    const feedback = document.getElementById("feedback")
+
+    if (selected.value == null) {
+        feedback.textContent = "Please select an answer!";
+        feedback.style.color = "orange";
+        return;
+    }
+
+    if (selected.value === correctAnswer) {
+        feedback.textContent = "Correct. Good job!"
+        feedback.style.color = "green"
+    } else {
+        feedback.textContent = "Wrong. Please try again"
+        feedback.style.color = "red"
+    }
+}
+
+document.getElementById("submit-answer").addEventListener('click', checkAnswer)
